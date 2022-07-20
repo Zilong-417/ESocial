f4441978200f09274ce05109738fd3489915dd6d
Zilong-417 <2627973708@qq.com>
2022-07-20 22:28:52 +0800

    完善首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,13 +1,21 @@
 <template>
 	<view>
 		<!--导航栏-->
-		<scroll-view scroll-x class="scroll-row border-bottom border-light-secondary" :scroll-into-view="scrollInto" scroll-with-animation>
+		<scroll-view scroll-x class="scroll-row border-bottom border-light-secondary sw" 
+		:scroll-into-view="scrollInto" 
+		scroll-with-animation>
 			<view v-for="(item,index) in tabList" :key="index"
 			class="scroll-row-item px-3 py-2 font-md" :id="'tab'+index"
 			:class="tabIndex===index?'text-main font-lg font-weight-bold':''"
 			@click="changeTab(index)">{{item.name}}</view>
-			
 		</scroll-view>
+		<swiper :duration="125" :current="tabIndex" @change="onChangeTab" :key="index" :style="'height:'+scrollH+'px'">
+			<swiper-item v-for="(item,index) in tabList" >
+				<scroll-view scroll-y :style="'height:'+scrollH+'px'">
+					<view v-for="i in 100" :key="i">{{i}}</view>
+				</scroll-view>
+			</swiper-item>
+		</swiper>
 		<!-- <block v-for="(item,index) in list" :key="index">
 			<CommonList :item='item' :index="index" @follow="follow" @doSupport='doSupport' ></CommonList>
 			<divider></divider>
@@ -20,6 +28,8 @@
 	export default {
 		data() {
 			return {
+				//列表高度
+				scrollH:600,
 				scrollInto:'',
 				tabIndex:0,
 				tabList:[{
@@ -78,7 +88,11 @@
 			}
 		},
 		onLoad() {
-
+			uni.getSystemInfo({
+				success: (res) => {
+					this.scrollH=res.windowHeight-uni.upx2px(100)
+				}
+			})
 		},
 		methods: {
 			//切换选项
@@ -117,6 +131,10 @@
 					item.support.unsupport_count--
 				}
 				item.support.type=e.type
+			},
+			//监听滑动
+			onChangeTab(e){
+				this.changeTab(e.detail.current)
 			}
 		},
 		components:{
@@ -126,5 +144,7 @@
 </script>
 
 <style lang="scss" scoped>
-	
+	.sw{
+		height: 100rpx;
+	}
 </style>
